252fd301984975c20f628244a01a54804f1107c1
Mateo <matuserafini@gmail.com>
2024-09-30 09:45:19 -0300

    db connect

---
+++ src/controller/book.controller.js
@@ -0,0 +1,21 @@
+import { bookModel } from "../models/book.model.js";
+
+export const getBooks =  async (req,res) => {
+
+};
+
+export const getBook =  async (req,res) => {
+
+};
+
+export const createBook =  async (req,res) => {
+
+};
+
+export const updateBooks =  async (req,res) => {
+
+};
+
+export const deleteBook =  async (req,res) => {
+
+};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
@@ -24,6 +24,6 @@ const schema = new mongoose.Schema(
   }
 );
 
-const userModel = mongoose.model("User", schema);
+const userModel = mongoose.model("User", userSchema);
 
 export default userModel;
